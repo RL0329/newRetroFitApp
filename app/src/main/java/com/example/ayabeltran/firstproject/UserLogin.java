c564226c8f0b082df8d1bc1c3e0db6b8d43d64d9
nzo <lorenzosarms11@gmail.com>
2018-03-08 17:36:30 +0800

    working login

--- app/src/main/java/com/example/ayabeltran/firstproject/UserLogin.java
+++ app/src/main/java/com/example/ayabeltran/firstproject/UserLogin.java
@@ -10,13 +10,23 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.*;
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class UserLogin extends AppCompatActivity {
 
     dbhelper mydb;
 
     public static EditText etloginame, etloginpword;
     Button btn_login;
-    SQLiteDatabase sqLiteDatabase;
+//    SQLiteDatabase sqLiteDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,26 +47,91 @@ public class UserLogin extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        mydb = new dbhelper(UserLogin.this);
-                        sqLiteDatabase = mydb.getReadableDatabase();
+
                         String loginame = etloginame.getText().toString();
                         String loginpword = etloginpword.getText().toString();
-                        Cursor res = mydb.userlogin(loginame, loginpword, sqLiteDatabase);
 
-                        if(res.moveToFirst()){
-                            Intent intent = new Intent(UserLogin.this,List.class);
-                            startActivity(intent);
 
-                            Toast.makeText(UserLogin.this, "welcome "+loginame+"!", Toast.LENGTH_SHORT).show();
+                        if(loginame.isEmpty()){
+                            Toast.makeText(UserLogin.this, "enter a username", Toast.LENGTH_LONG).show();
+                            etloginame.setText("");
+                            etloginame.requestFocus();
                         }
-                            else {
-                                Toast.makeText(UserLogin.this, "your username and password do not match!", Toast.LENGTH_LONG).show();
-                                etloginame.setText("");
-                                etloginpword.setText("");
-                                etloginame.requestFocus();
-                            }
+                        if(loginpword.isEmpty()){
+                            Toast.makeText(UserLogin.this, "enter a password", Toast.LENGTH_LONG).show();
+                            etloginpword.setText("");
+                            etloginpword.requestFocus();
+                        }
+
+                        serverLogin();
+
+
+//                        mydb = new dbhelper(UserLogin.this);
+//                        sqLiteDatabase = mydb.getReadableDatabase();
+//                        Cursor res = mydb.userlogin(loginame, loginpword, sqLiteDatabase);
+//
+//                        if(res.moveToFirst()){
+//                            Intent intent = new Intent(UserLogin.this,List.class);
+//                            startActivity(intent);
+//
+//                            Toast.makeText(UserLogin.this, "welcome "+loginame+"!", Toast.LENGTH_SHORT).show();
+//                        }
+//                            else {
+//                                Toast.makeText(UserLogin.this, "your username and password do not match!", Toast.LENGTH_LONG).show();
+//                                etloginame.setText("");
+//                                etloginpword.setText("");
+//                                etloginame.requestFocus();
+//                            }
+
                     }
                 }
         );
+    }
+
+    private void serverLogin(){
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl(create_user.baseURL)
+                .addConverterFactory(GsonConverterFactory.create());
+
+        Retrofit retrofit = builder.client(httpClient.build()).build();
+
+        gitHubClient client = retrofit.create(gitHubClient.class);
+        Call<java.util.List<GitHubRepo>> call = client.logIn(etloginame.getText().toString(),etloginpword.getText().toString());
+
+
+        call.enqueue(new Callback<java.util.List<GitHubRepo>>() {
+            @Override
+            public void onResponse(Call<List<GitHubRepo>> call, Response<List<GitHubRepo>> response) {
+                List<GitHubRepo> mLogIn = response.body();
+                if(mLogIn.isEmpty()){
+                    Toast.makeText(UserLogin.this,"your login credentials do not match",Toast.LENGTH_SHORT).show();
+                    etloginame.setText("");
+                    etloginpword.setText("");
+                    etloginame.requestFocus();
+                }
+                else{
+                    Toast.makeText(UserLogin.this,"they match",Toast.LENGTH_SHORT).show();
+
+                    Intent i = new Intent(UserLogin.this, com.example.ayabeltran.firstproject.List.class);
+                    startActivity(i);
+
+
+
+//                    Intent i = new Intent(UserLogin.this,List.class);
+//                    startActivity(i);
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<GitHubRepo>> call, Throwable t) {
+
+                Toast.makeText(UserLogin.this,"error in login",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
