400bb328955136bdf4a1041a6c88b03b3fe380b0
Aya <ayabeltran14@gmail.com>
2018-03-15 16:29:39 +0800

    trying to add pagination,gaad magawa ka na

--- app/src/main/java/com/example/ayabeltran/firstproject/gitHubClient.java
+++ app/src/main/java/com/example/ayabeltran/firstproject/gitHubClient.java
@@ -23,19 +23,22 @@ public interface gitHubClient {
     );
 
     @GET("/posts")
-    ////////////////////////////////method/////////column///////////value
+        ////////////////////////////////method/////////column///////////value
     Call<java.util.List<GitHubRepo>> eUser(@Query("username") String name);
 
 
     @GET("/posts")
     Call<java.util.List<GitHubRepo>> logIn(@Query("username")String uname, @Query("password") String pword);
 
-    @GET("{comments}")
+    @GET("{comments}?_sort=id&_order=desc&_limit=5")
     Call<java.util.ArrayList<ImgRepo>> displayimg(@Path("comments") String comments);
 
     @POST("posts")
     Call<GitHubRepo>
     adduser(@Body GitHubRepo posts);
+
+    @GET("{comments}?_sort=id&_order=desc&_limit=5&_offset=5")
+    Call<java.util.ArrayList<ImgRepo>>fetchNewData (@Path("comments") String comments);
 
     @POST("/comments")Call<ImgRepo> addimg(@Body ImgRepo value);
 
