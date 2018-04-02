1b9d18461c54c17a1eba2cc6833460cf101d2a7a
Aya <ayabeltran14@gmail.com>
2018-04-02 09:19:17 +0800

    working onlick in listdisplay

--- app/src/main/java/com/example/ayabeltran/firstproject/ListDisplay.java
+++ app/src/main/java/com/example/ayabeltran/firstproject/ListDisplay.java
@@ -8,6 +8,8 @@ import android.util.Base64;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 public class ListDisplay extends AppCompatActivity {
 
     ImageView image;
@@ -26,11 +28,16 @@ public class ListDisplay extends AppCompatActivity {
         Bundle extra = getIntent().getExtras();
         String  Key = extra.getString("Key");
         String  Key2 = extra.getString("Key2");
-        String  Key3 = extra.getString("key3");
+        String  Key3 = extra.getString("Key3");
 //        byte[]  Key3 = extra.getByteArray("Key3");
+
 
         name.setText(Key);
         description.setText(Key2);
+        Picasso.get()
+                .load(Key3)
+                .placeholder(R.drawable.ic_launcher_background)
+                .into(image);
 
 //        Bitmap bm = BitmapFactory.decodeByteArray(Key3, 0, Key3.length);
 //        image.setImageBitmap(bm);
